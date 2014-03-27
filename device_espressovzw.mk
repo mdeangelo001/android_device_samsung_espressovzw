f137866e0b9709e9d3fba954ce83b1e2bf5e72ee
Jason Riordan <jriordan001@gmail.com>
2014-03-27 12:46:32 -0400

    Import msm8960 GPS from d2lte, update board flags for aosp audio/media HW libs

--- device_espressovzw.mk
+++ device_espressovzw.mk
@@ -1,21 +1,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
-
 $(call inherit-product-if-exists, vendor/samsung/espressovzw/espressovzw-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/samsung/espressovzw/overlay
 
-LOCAL_PATH := device/samsung/espressovzw
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
+# GPS
+PRODUCT_PACKAGES += \
+    gps.msm8960
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
+    device/samsung/espressovzw/gps/gps.conf:system/etc/gps.conf
 
 $(call inherit-product, build/target/product/full.mk)
 
