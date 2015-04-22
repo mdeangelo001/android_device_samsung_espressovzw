c0d1479904b3e603c04697763e5a85eb7f77c199
jrior001 <jriordan001@gmail.com>
2015-04-22 17:34:55 -0400

    update BT configs
    - update bt script (kang from htc 8960's
    - remove SMD_TTY
    - clean up init
    
    Change-Id: I9b77fe56239e90c0a5896c21ba94415ef121bf3e

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -21,10 +21,5 @@
 #define BTM_DEF_LOCAL_NAME	"Galaxy Tab 2"
 #define BTA_DISABLE_DELAY 1000	/* in milliseconds */
 #define BLUETOOTH_QTI_SW TRUE
-#define BTA_HOST_INTERLEAVE_SEARCH TRUE
-// skips conn update at conn completion
-#define BTA_BLE_SKIP_CONN_UPD TRUE
-// Disables read remote device feature
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT TRUE
 
 #endif
